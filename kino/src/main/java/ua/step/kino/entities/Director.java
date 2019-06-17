6c44aab216fa394cf4861a896b5b12c6333ea44a
Zavo_yy45 <Zavo_yy45@1-9D.od.itstep.local>
2019-06-17 21:04:38 +0300

    Added Entity Director

---
+++ kino/src/main/java/ua/step/kino/entities/Director.java
@@ -0,0 +1,20 @@
+package ua.step.kino.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "directors")
+public class Director {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "author_id", unique = true, nullable = false)
+	private Long id;
+
+	private String name;
+	
+}
