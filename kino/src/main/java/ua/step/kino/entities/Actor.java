957340438b0149d8ca254275ea81babb11826fa1
Kojevin <Kojevin@210-6P.od.itstep.local>
2019-06-18 18:52:09 +0300

    Improved class Actor!

--- kino/src/main/java/ua/step/kino/entities/Actor.java
+++ kino/src/main/java/ua/step/kino/entities/Actor.java
@@ -1,10 +1,98 @@
 package ua.step.kino.entities;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 /**
  * 
  * @author Kojevin
  *
  */
+@Entity
+@Table(name = "actors")
 public class Actor {
+	
+	/*
+	 * fields
+	 */
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	@JsonFormat(pattern = "dd-MM-yyyy")
+	private Date dateOfBirthday;
+	
+	private Country country;
+	
+	@OneToMany(mappedBy = "actor")
+	private List<Film> films;
+	
+	/*
+	 * functions
+	 */
+	
+	public Actor()
+	{
+		
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDateOfBirthday() {
+		return dateOfBirthday;
+	}
+
+	public void setDateOfBirthday(Date dateOfBirthday) {
+		this.dateOfBirthday = dateOfBirthday;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public List<Film> getFilms() {
+		return films;
+	}
+
+	public void setFilms(List<Film> films) {
+		this.films = films;
+	}
+	
+	
+	
 }
