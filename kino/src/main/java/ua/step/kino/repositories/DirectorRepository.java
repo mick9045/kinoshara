ea2e3e7b76853fff73e15886308969259abe8af4
AZavoruyev <azavoruyev@gmail.com>
2019-06-18 16:13:17 +0300

    method for searcing Director by name and part of name added to DirectorRepository

--- kino/src/main/java/ua/step/kino/repositories/DirectorRepository.java
+++ kino/src/main/java/ua/step/kino/repositories/DirectorRepository.java
@@ -1,11 +1,24 @@
 package ua.step.kino.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ua.step.kino.entities.Director;
-
+/**
+ * 
+ * @author AZavoruyev
+ *
+ */
 
 
 public interface DirectorRepository extends JpaRepository<Director, Integer>{
-
+//поиск режисера по имени
+	 @Query("select b from Director b where b.name = :name")
+	 Director findByName(@Param("name") String name);
+	//поиск режисера по фрагменту имени
+	  @Query("select b from Director b where b.name like %:name%")
+	  List<Director> findNameContains(@Param("name") String name);
 }
