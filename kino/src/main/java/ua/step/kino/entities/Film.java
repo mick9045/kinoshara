4e0398a1a5640b0dea3c2abd87eb0ead11fc25ae
Aleksei Glodovskij <glodovskii@gmail.com>
2019-06-18 16:24:13 +0300

    Complete entity Film

--- kino/src/main/java/ua/step/kino/entities/Film.java
+++ kino/src/main/java/ua/step/kino/entities/Film.java
@@ -1,5 +1,92 @@
 package ua.step.kino.entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+/**
+ * 
+ * @author Aleksey
+ *
+ */
+
+@Entity
+@Table(name = "films")
 public class Film {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	@JoinColumn(name = "director_id")
+	private Set<Director> directors = new HashSet<Director>();
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	@JoinColumn(name = "actor_id")
+	private Set<Actor> actors = new HashSet<Actor>();
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	@JoinColumn(name = "genre_id")
+	private Set<Genre> genres = new HashSet<Genre>();
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+	@JoinColumn(name = "country_id")
+	private Set<Country> countries = new HashSet<Country>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Set<Director> getDirectors() {
+		return directors;
+	}
+
+	public void setDirectors(Set<Director> directors) {
+		this.directors = directors;
+	}
+
+	public Set<Actor> getActors() {
+		return actors;
+	}
+
+	public void setActors(Set<Actor> actors) {
+		this.actors = actors;
+	}
+
+	public Set<Genre> getGenres() {
+		return genres;
+	}
+
+	public void setGenres(Set<Genre> genres) {
+		this.genres = genres;
+	}
+
+	public Set<Country> getCountries() {
+		return countries;
+	}
+
+	public void setCountries(Set<Country> countries) {
+		this.countries = countries;
+	}
+	
+	
+	
 
 }
