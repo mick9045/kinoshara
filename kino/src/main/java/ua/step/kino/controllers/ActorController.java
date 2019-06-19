db453b116e7a22620e64594bc14630a437919a27
Fessa <Kojevin83@gmail.com>
2019-06-19 19:56:46 +0300

    Added actor files!!!

---
+++ kino/src/main/java/ua/step/kino/controllers/ActorController.java
@@ -0,0 +1,34 @@
+package ua.step.kino.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ua.step.kino.entities.Actor;
+import ua.step.kino.repositories.ActorRepository;
+
+/**
+ * 
+ * @author Kojevin
+ *
+ */
+
+@Controller
+@RequestMapping("/actors")
+public class ActorController 
+{
+	@Autowired ActorRepository actorRepository;
+	
+	@GetMapping
+	public String showAll(Model model) 
+	{
+		List<Actor> actors = actorRepository.findAll();
+		model.addAttribute("actors", actors);
+		return "actors";
+	}
+}
+
---
+++ kino/src/main/resources/data-Actors.sql
@@ -0,0 +1,3 @@
+insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Крис', 'Хемсворт', '11-08-1983', null, null)
+insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Скарлетт', 'Йоханссон', '22-11-1984', null, null)
+insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Крис', 'Эванс', '13-07-1981', null, null)
