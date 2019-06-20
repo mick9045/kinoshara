0c6357098b8ea6c6d2589cb56cb15296f32d15d4
Fessa <Kojevin83@gmail.com>
2019-06-21 00:05:19 +0300

    add all files about Actor!

--- kino/src/main/resources/data-Actors.sql
+++ kino/src/main/resources/data-Actors.sql
@@ -1,3 +1,9 @@
-insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Крис', 'Хемсворт', '11-08-1983', null, null)
-insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Скарлетт', 'Йоханссон', '22-11-1984', null, null)
-insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Крис', 'Эванс', '13-07-1981', null, null)
+insert into films(id, title) VALUES (1, 'Alise')
+
+insert into Countries (id, name) VALUES(1, 'USA')
+insert into Actors (id, first_Name, last_Name, date_Of_Birthday, country_id) VALUES(1, 'Крис', 'Хемсворт', '1983-08-11', 1)
+
+insert into Actors (id, first_Name, last_Name, date_Of_Birthday, country_id) VALUES(2, 'Скарлетт', 'Йоханссон', '1984-11-22', 1)
+insert into Actors (id, first_Name, last_Name, date_Of_Birthday, country_id) VALUES(3, 'Крис', 'Эванс', '1981-07-13', 1)
+
+insert into ACTORS_FILMS (films_id, actor_id) VALUES (1,1)
--- kino/src/main/resources/data.sql
+++ kino/src/main/resources/data.sql
@@ -1,7 +1,8 @@
-insert into Countries (name) VALUES('Ukraine')
-insert into Countries (name) VALUES('USA')
-insert into Countries (name) VALUES('Poland')
-insert into Countries (name) VALUES('Russia')
-insert into Actors (firstname, lastname, dateofbirthday, country, films) VALUES('Крис', 'Хемсворт', '11-08-1983', null, null)
-<!--insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Скарлетт', 'Йоханссон', '22-11-1984', null, null)-->
-<!--insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Крис', 'Эванс', '13-07-1981', null, null)-->
+select 1
+--insert into Countries (name) VALUES('Ukraine')
+--insert into Countries (name) VALUES('USA')
+--insert into Countries (name) VALUES('Poland')
+--insert into Countries (name) VALUES('Russia')
+--insert into Actors (firstname, lastname, dateofbirthday, country, films) VALUES('Крис', 'Хемсворт', '11-08-1983', null, null)
+--insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Скарлетт', 'Йоханссон', '22-11-1984', null, null)
+--insert into Actors (firstName, lastName, dateOfBirthday, country, films) VALUES('Крис', 'Эванс', '13-07-1981', null, null)
