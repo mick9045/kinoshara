43c5953e7fcc2a51f11681b4de774148025ce4a0
Julia <Julia@10.4.5.202>
2019-06-20 19:13:36 +0300

    Added GenreController and GenreRepository

---
+++ kino/src/main/java/ua/step/kino/repositories/GenreRepository.java
@@ -0,0 +1,14 @@
+package ua.step.kino.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ua.step.kino.entities.Genre;
+
+/**
+ * 
+ * @author Julia
+ *
+ */
+public interface GenreRepository extends JpaRepository<Genre, Integer>{
+
+}
