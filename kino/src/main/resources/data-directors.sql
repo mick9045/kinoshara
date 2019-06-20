4c0be04954996d5a934be81e0d900fc4afa3e260
AZavoruyev <azavoruyev@gmail.com>
2019-06-20 16:51:29 +0300

    Changes fields names in Director Added data-directors to fill DB Corrected directors.html

---
+++ kino/src/main/resources/data-directors.sql
@@ -0,0 +1,4 @@
+insert into directors (name, birthday, country_id) VALUES ('John King', '1965-03-12', 1)
+insert into directors (name, birthday, country_id) VALUES ('Vill Smight', '1973-11-15', 2)
+insert into directors (name, birthday, country_id) VALUES ('Gordon Din', '1986-02-01', 4)
+insert into directors (name, birthday, country_id) VALUES ('John Smight', '1991-10-21', 3)
