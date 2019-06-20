b30bc8ccc4ea0839682967ab2211031aefca7bd0
AZavoruyev <azavoruyev@gmail.com>
2019-06-20 20:38:55 +0300

    Added data to data-directors

--- kino/src/main/resources/data-directors.sql
+++ kino/src/main/resources/data-directors.sql
@@ -5,8 +5,20 @@ insert into directors (id, name, birthday, country_id) VALUES (4,'John Smight', 
 
 insert into films(id, title) VALUES (1,'Armaggedon')
 insert into films(id, title) VALUES (2,'Averangrs')
-insert into films(id, title) VALUES (3, 'asdfs')
+insert into films(id, title) VALUES (3, 'Green Mile')
+insert into films(id, title) VALUES (4, 'Long way home')
+insert into films(id, title) VALUES (5, 'Midnight sun')
+insert into films(id, title) VALUES (6, 'Man of steel')
+insert into films(id, title) VALUES (7, 'Kingdom')
+insert into films(id, title) VALUES (8, 'Alise')
+
 
 insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (1,2)
 insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (2,2)
 insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (3,2)
+insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (4,1)
+insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (5,1)
+insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (6,4)
+insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (7,4)
+insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (8,3)
+
