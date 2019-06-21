41b8fb3d4e0dc45aa2ce97dccceea4ff7b2c3cf9
Kojevin <Kojevin@210-6P.od.itstep.local>
2019-06-21 19:50:38 +0300

    last commit!

--- kino/src/main/resources/data-countries.sql
+++ kino/src/main/resources/data-countries.sql
@@ -1,4 +1,4 @@
-insert into Countries (name) VALUES('Ukraine')
 insert into Countries (name) VALUES('USA')
+insert into Countries (name) VALUES('Ukraine')
 insert into Countries (name) VALUES('Poland')
 insert into Countries (name) VALUES('Russia')
