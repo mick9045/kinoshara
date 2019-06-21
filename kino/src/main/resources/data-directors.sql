a863f413a4066b0674cd2305965f49c298005413
mick9045 <mick9045@gmail.com>
2019-06-21 14:49:30 +0300

    dirty fix

--- kino/src/main/resources/data-directors.sql
+++ kino/src/main/resources/data-directors.sql
@@ -22,3 +22,9 @@ insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (6,4)
 insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (7,4)
 insert into DIRECTORS_FILMS  (films_id, director_id) VALUES (8,3)
 
+insert into Actors (id, first_Name, last_Name, date_Of_Birthday, country_id) VALUES(1, 'Крис', 'Хемсворт', '1983-08-11', 1)
+
+insert into Actors (id, first_Name, last_Name, date_Of_Birthday, country_id) VALUES(2, 'Скарлетт', 'Йоханссон', '1984-11-22', 1)
+insert into Actors (id, first_Name, last_Name, date_Of_Birthday, country_id) VALUES(3, 'Крис', 'Эванс', '1981-07-13', 1)
+
+insert into ACTORS_FILMS (films_id, actor_id) VALUES (1,1)
