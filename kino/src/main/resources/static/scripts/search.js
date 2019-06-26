3758e4552bc6273ea4ccee6e8944be59a0322bbe
mick9045 <mick9045@gmail.com>
2019-06-26 21:36:41 +0300

    Added autocomplete!

---
+++ kino/src/main/resources/static/scripts/search.js
@@ -0,0 +1,24 @@
+$(function() {
+	var searchButton = $("#searchButton");
+	var searchTextBox = $("#searchTextBox");
+	
+	searchTextBox.autocomplete({
+		source: function(request, response) {
+			$.get( "/search/" + request.term, function(data) {
+				var titles = [data.length];
+				for (var i = 0; i < data.length; i++) {
+					titles[i] = {
+							label: data[i].title,
+							value: data[i].title,
+							data: data[i]
+					};
+				}
+				for (var i = 0; i < titles.length; i++) {
+					response(titles);
+				}
+			});
+		},
+		select: function(event, ui) {
+		}
+	});
+})
