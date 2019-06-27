c194cf9a0ca8387a221ad8d606ab4e7551ea4bbe
Aleksei Glodovskij <glodovskii@gmail.com>
2019-06-27 13:25:59 +0300

    Commit of concrete film which shows that everithing is works

--- kino/src/main/java/ua/step/kino/controllers/FilmController.java
+++ kino/src/main/java/ua/step/kino/controllers/FilmController.java
@@ -1,12 +1,16 @@
 package ua.step.kino.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ua.step.kino.entities.Film;
 import ua.step.kino.repositories.FilmRepository;
@@ -28,6 +32,13 @@ public class FilmController {
 		List<Film> films = filmsRepository.findAll();
 		model.addAttribute("films", films);
 		return "allMovies";
+	}
+	
+	@GetMapping("{id}")
+	public String showOne(@PathVariable int id, Model model)
+	{
+		filmsRepository.findById(id).ifPresent(o -> model.addAttribute("film", o));
+		return "Movie";
 	}
 
 }
