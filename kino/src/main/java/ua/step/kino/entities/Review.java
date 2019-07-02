3f957620a28db0067069cfe1a84793c88e759bc7
AZavoruyev <azavoruyev@gmail.com>
2019-07-02 20:33:41 +0300

    Added data-reviews /reviews.html corrected/Added new user / Review corrected

--- kino/src/main/java/ua/step/kino/entities/Review.java
+++ kino/src/main/java/ua/step/kino/entities/Review.java
@@ -24,7 +24,7 @@ public class Review {
 	private int id;
 	
 	@ManyToOne(fetch=FetchType.LAZY)
-	@JoinColumn(name="user_id")
+	@JoinColumn(name="USER_ID")
 	private User user;
 	
 	@ManyToOne(fetch=FetchType.LAZY)
