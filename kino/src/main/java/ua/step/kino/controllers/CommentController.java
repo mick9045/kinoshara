25a29791b8b828e9155f64f53f33e613a4944622
Julia <Julia@DESKTOP-L7QI5K5>
2019-07-05 12:00:29 +0300

    Fixed

--- kino/src/main/java/ua/step/kino/controllers/CommentController.java
+++ kino/src/main/java/ua/step/kino/controllers/CommentController.java
@@ -3,7 +3,6 @@ package ua.step.kino.controllers;
 import java.util.Date;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import ua.step.kino.entities.Comment;
 import ua.step.kino.repositories.CommentRepository;
-import ua.step.kino.services.CommentService;
+import ua.step.kino.repositories.FilmRepository;
+import ua.step.kino.repositories.UsersRepository;
 
 /**
  * 
@@ -24,24 +24,35 @@ import ua.step.kino.services.CommentService;
 @Controller
 @RequestMapping("/comments")
 public class CommentController {
-	@Autowired 
-	CommentRepository commentRepository;
 	
-	@Autowired(required = false)
-	CommentService commentService;
-//	@GetMapping
-//	public String showAll(Model model) {
-//		List<Comment> comments = commentRepository.findAll();
-//		model.addAttribute("comments", comments);
-//		return "comments";
-//	}
+	@Autowired
+	private CommentRepository commentRepository;
+	@Autowired
+	private FilmRepository filmRepository;
+	@Autowired
+	private UsersRepository userRepository;
+	
+	@GetMapping
+	public String showAll(Model model) {
+		List<Comment> comment = commentRepository.findAll();
+		model.addAttribute("coments", comment);
+		return "comments";
+	}
 	
 	@RequestMapping(value = "/addComment", method = RequestMethod.POST)
-	private String addComment(@ModelAttribute("comments") Comment comment) {
+	private String addComment(Model model, Integer filmId, Integer userId, String text) {
+		
+		
+		Comment comment = new Comment();
+		comment.setFilm(filmRepository.findById(filmId).get());
+		comment.setUser(userRepository.findById(userId).get());
+		comment.setText(text);
 		comment.setDate(new Date());
 		
-		commentService.Add(comment);
-	
-		return "redirect:/Movie/";
+		commentRepository.save(comment);
+		
+		//System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!" + comment.getText() + comment.getUser()+ comment.getFilm() + comment.getDate());
+
+		return "redirect:/films/" + comment.getFilm().getId();
 	}
 }
--- kino/src/main/java/ua/step/kino/services/CommentService.java
+++
@@ -1,7 +0,0 @@
-package ua.step.kino.services;
-
-import ua.step.kino.entities.Comment;
-
-public interface CommentService {
-	Comment Add(Comment comment);
-}
