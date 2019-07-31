76c20646bec6482b386274358a1da5e7730bda00
User <User@WIN-MRSE9V5VMOH>
2019-07-31 18:10:19 +0300

    CRUD operations for countries and genres with existed check finished

--- kino/src/main/java/ua/step/kino/controllers/CountryController.java
+++ kino/src/main/java/ua/step/kino/controllers/CountryController.java
@@ -1,7 +1,7 @@
 package ua.step.kino.controllers;
 
-
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import ua.step.kino.entities.Country;
 import ua.step.kino.repositories.CountryRepository;
 
@@ -25,62 +27,63 @@ import ua.step.kino.repositories.CountryRepository;
 @Controller
 @RequestMapping("/countries")
 public class CountryController {
-	@Autowired CountryRepository countryRepository;
-	
+	@Autowired
+	CountryRepository countryRepository;
+
 	@GetMapping
 	public String showAll(Model model) {
 		List<Country> countries = countryRepository.findAll();
 		model.addAttribute("countries", countries);
 		return "countries";
 	}
-	
+
 	@GetMapping("/add")
-    public String showAddForm(Country countrie) {
-        return "add-countrie";
-    }
-     
-    @PostMapping("/add")
-    public String addEntity(@Valid Country country, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-countrie";
-        }
-         
-        countryRepository.save(country);
-        List<Country> countries = countryRepository.findAll();
-		model.addAttribute("countries", countries);
-		return "countries";
-    }
-	
+	public String showAddForm(Country country) {
+		return "country-add";
+	}
+
+	@PostMapping("/add")
+	public String addEntity(@Valid Country country, @RequestParam Optional<String> error, BindingResult result, Model model) {
+		if (countryRepository.findByName(country.getName().toLowerCase()) != null) {
+			model.addAttribute("error", "This country is already exist!");
+			return "country-add";
+		}
+		if (result.hasErrors()) {
+			return "country-add";
+		}
+		countryRepository.save(country);
+		return "redirect:/countries";
+	}
+
 	@GetMapping("/edit/{id}")
 	public String showUpdateForm(@PathVariable("id") int id, Model model) {
 		Country country = countryRepository.findById(id)
-	      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-	     
-	    model.addAttribute("country", country);
-	    return "update-country";
+				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+		model.addAttribute("country", country);
+		return "country-edit";
 	}
-	
+
 	@PostMapping("/edit/{id}")
-	public String updateEntity(@PathVariable("id") int id, @Valid Country country, 
-	  BindingResult result, Model model) {
-	    if (result.hasErrors()) {
-	    	country.setId(id);
-	        return "update-country";
-	    }
-	         
-	    countryRepository.save(country);
-        List<Country> countries = countryRepository.findAll();
-		model.addAttribute("countries", countries);
-		return "countries";
+	public String updateEntity(@PathVariable("id") int id, @RequestParam Optional<String> error, @Valid Country country, BindingResult result, Model model) {
+		if (countryRepository.findByName(country.getName().toLowerCase()) != null) {
+			model.addAttribute("error", "This country is already exist!");
+			return "country-edit";
+		}
+		if (result.hasErrors()) {
+			country.setId(id);
+			return "country-edit";
+		}
+
+		countryRepository.save(country);
+		return "redirect:/countries";
 	}
-	     
+
 	@GetMapping("/delete/{id}")
-	public String deleteUser(@PathVariable("id") int id, Model model) {
+	public String deleteEntity(@PathVariable("id") int id, Model model) {
 		Country country = countryRepository.findById(id)
-	      .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
 		countryRepository.delete(country);
-		List<Country> countries = countryRepository.findAll();
-		model.addAttribute("countries", countries);
-		return "countries";
+		return "redirect:/countries";
 	}
 }
